f048bc83e29f4af2a8ca4f3b088a1baf81aa6de8
Misha Kukarkin <65669569+Wardenclock1759@users.noreply.github.com>
2023-08-12 19:19:08 +0500

    Delete src directory

--- src/assets/constants/currency.constants.ts
+++
@@ -1,9 +0,0 @@
-export const CURRENCY_CODES = {
-  RUB: "RUB",
-  USD: "USD",
-  EUR: "EUR",
-  GBP: "GBP",
-  CNY: "CNY",
-  JPY: "JPY",
-  TRY: "TRY",
-};
--- src/assets/constants/text.constants.ts
+++
@@ -1,3 +0,0 @@
-export const TEXT_ENG = {
-  TITLE: "Currency RUB",
-};
